f8e5b190e00b52569682e5bf4c1f3b26f43abae1
heckardkyle <heckardkyle@yahoo.com>
2025-02-24 21:33:36 -0800

    Added LeafNode class and more unit test

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -42,6 +42,34 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode(props=None)
         string = ""
         self.assertEqual(string, node.props_to_html())
+
+    def test_LeafNode_props_to_html(self):
+        dict = {}
+        dict["key1"] = "value1"
+        dict["key2"] = "value2"
+        node = LeafNode("p", "value", dict)
+        string = 'key1="value1" key2="value2"'
+        self.assertEqual(string, node.props_to_html())
+
+    def test_LeafNode(self):
+        node = LeafNode(None, "value")
+        string = "value"
+        self.assertEqual(string, node.to_html())
+
+    def test_LeafNode2(self):
+        node = LeafNode(None, "value", {"k": "v"})
+        string = "value"
+        self.assertEqual(string, node.to_html())
+
+    def test_LeafNode3(self):
+        node = LeafNode("p", "value")
+        string = "<p>value</p>"
+        self.assertEqual(string, node.to_html())
+
+    def test_LeafNode4(self):
+        node = LeafNode("a", "value", {"k1": "v1", "k2": "v2"})
+        string = '<a k1="v1" k2="v2">value</a>'
+        self.assertEqual(string, node.to_html())
 
 
 if __name__ == "__main__":
