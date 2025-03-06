b856be2b35e57c021ef86d8cc160c3fac78360f1
heckardkyle <heckardkyle@yahoo.com>
2025-03-05 23:34:41 -0800

    Added methods to main.py to copy static files to public

--- src/main.py
+++ src/main.py
@@ -1,6 +1,32 @@
+import os
+import shutil
 from textnode import TextType, TextNode
 
+public_path = "./public/"
+static_path = "./static/"
+
 def main():
-    pass
+    generate_public_files()
+
+def generate_public_files():
+    if not os.path.exists(static_path):
+        raise Exception("static/ does not exist")
+    if os.path.exists(public_path):
+        shutil.rmtree(public_path)
+    os.mkdir(public_path)
+    copy_static_files(static_path ,os.listdir(static_path))
+    
+def copy_static_files(path, files):
+    if files == []:
+        return
+    for file in files:
+        if os.path.isfile(path + file):
+            shutil.copy(path + file, public_path + path[len(static_path):])
+        else:
+            new_dir = public_path + path[len(static_path):] + file + "/"
+            os.mkdir(new_dir)
+            copy_static_files(path + file + "/", os.listdir(path + file + "/"))
+    
+    return
 
 main()
