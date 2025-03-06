dbd36a7e714f4f47b8228db19f5fd2ef6f5e7930
heckardkyle <heckardkyle@yahoo.com>
2025-03-06 00:10:49 -0800

    improved code in main.py

--- src/main.py
+++ src/main.py
@@ -2,30 +2,38 @@ import os
 import shutil
 from textnode import TextType, TextNode
 
-public_path = "./public/"
-static_path = "./static/"
+public_path = os.path.join(".", "public")
+static_path = os.path.join(".", "static")
 
 def main():
     generate_public_files()
 
 def generate_public_files():
     if not os.path.exists(static_path):
-        raise Exception("static/ does not exist")
+        raise Exception(static_path + " does not exist")
     if os.path.exists(public_path):
         shutil.rmtree(public_path)
-    os.mkdir(public_path)
+    os.makedirs(public_path, exist_ok=True)
     copy_static_files(static_path ,os.listdir(static_path))
     
 def copy_static_files(path, files):
     if files == []:
         return
     for file in files:
-        if os.path.isfile(path + file):
-            shutil.copy(path + file, public_path + path[len(static_path):])
+        file_path = os.path.join(path, file)
+        relative_path = os.path.relpath(path, static_path)
+
+        if os.path.isfile(file_path):
+            dest_dir = os.path.join(public_path, relative_path)
+            os.makedirs(dest_dir, exist_ok=True)
+            dest_file = os.path.join(dest_dir, file)
+            print(f"Copying {file_path} to {dest_file}")
+            shutil.copy(file_path, dest_file)
         else:
-            new_dir = public_path + path[len(static_path):] + file + "/"
-            os.mkdir(new_dir)
-            copy_static_files(path + file + "/", os.listdir(path + file + "/"))
+            new_dir = os.path.join(public_path, relative_path, file)
+            os.makedirs(new_dir, exist_ok=True)
+            
+            copy_static_files(file_path, os.listdir(file_path))
     
     return
 
